f1ead1124561fbc1024f5ddd47bd5ebc19f8762b
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2022-10-30 18:32:33 -0300

    comecei o projeto

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function tabuada() {
+    let numero = document.getElementById('numero')
+    let tabuadaArea = document.getElementById('tabuada-area')
+    if (numero.value.length == 0) {
+        alert('Digite um número!')
+    } else
+    let numeroValor = Number(numero.value)
+}
+
