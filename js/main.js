8cbeab6f5f46f928fbeae42568f787ec8a1b78ad
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2022-10-31 16:36:44 -0300

    projeto finalizado

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-function tabuada() {
-    let numero = document.getElementById('numero')
-    let tabuadaArea = document.getElementById('tabuada-area')
-    if (numero.value.length == 0) {
-        alert('Digite um número!')
-    } else
-    let numeroValor = Number(numero.value)
-}
+const botao = document.getElementById('botao')
+botao.addEventListener('click', clicar)
 
+const numero = document.getElementById('numero')
+const select = document.getElementById('select-tabu')
+
+function clicar() {
+    if (numero.value.length == 0) {
+        alert('Digite um Número')
+    } else {
+        const valor = Number(numero.value)//convertendo o numero string em numero
+        select.innerHTML = ''
+        for (let c=1; c<=10; c++) {
+            const item = document.createElement('option')
+            item.text = `${valor} x ${c} = ${valor*c}`
+            select.appendChild(item)
+        }
+    }
+        
+}
